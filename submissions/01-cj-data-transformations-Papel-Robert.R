ca2091226e1abece94d790a207bdbb573cabe2bb
Robert Papel <papelr@d-172-25-231-174.dhcp.virginia.edu>
2018-03-26 16:09:20 -0400

    Finished Complete Journey exercises - I have no idea about #5, and the posted solution makes no intuitive sense!

--- submissions/01-cj-data-transformations-Papel-Robert.R
+++ submissions/01-cj-data-transformations-Papel-Robert.R
@@ -23,16 +23,19 @@ transaction_data <- transaction_data %>%
     coupon_match_disc = abs(coupon_match_disc)
   )
 
-#### ----- Question 2: Create three new variables named regular_price, loyalty_price, and coupon_price according to the following logic:
+transaction_data
+
+#### ----- Question 2: Create three new variables named regular_price,
+#### loyalty_price, and coupon_price according to the following logic:
 
 transaction_data <- transaction_data %>%
-  mutate(regular_price = (sales_value + retail_disc + coupon_match_disc) / quantity) %>% 
+  mutate(regular_price = (sales_value + retail_disc + coupon_match_disc) /  quantity) %>% 
   mutate(loyalty_price = (sales_value + coupon_match_disc) / quantity) %>% 
   mutate(coupon_price  = (sales_value - coupon_disc) / quantity)
 
-# Question 3: transaction_data includes 92,339 unique product IDs. How many of
-# these products (not transactions!) had a regular price of one dollar or less?
-# What does this count equal for loyalty and coupon prices?
+#### ---- Question 3: transaction_data includes 92,339 unique product IDs. How
+#### many of these products (not transactions!) had a regular price of one
+#### dollar or less? What does this count equal for loyalty and coupon prices?
 
 transaction_data %>%
   filter(regular_price <= 1.00) %>%
@@ -42,34 +45,50 @@ transaction_data %>%
 # Answer: there are 12,442 products that are $1 or less
 
 transaction_data %>%
-  select(loyalty_price, product_id)  %>%
-  filter(loyalty_price < 1.00) %>%
-  summarise(count_all = n_distinct(product_id))
+  filter(loyalty_price <= 1.00) %>%
+  select(product_id)  %>%
+  n_distinct()
 
-# Answer: 
+# Answer: 20,113 
 
 transaction_data %>%
-  select(coupon_price, product_id)  %>%
-  filter(coupon_price < 1.00) %>%
-  summarise(count_all = n_distinct(product_id))
+  filter(coupon_price <= 1.00) %>%
+  select(product_id)  %>%
+  n_distinct()
 
-# Answer:
+# Answer: 22,273
 
 #### ----- Question 4: What proportion of baskets are over $10 in sales value?
 
 transaction_data %>%
-  select(sales_value, basket_id, product_id) %>%
-  group_by(sales_value, basket_id, product_id) %>%
-  summarise(transaction_data, basket_id, mean(basket_value > 10))
+  group_by(basket_id) %>%
+  summarize(basket_value = sum(sales_value)) %>%
+  ungroup() %>%
+  summarize(proportion_over_10 = mean(basket_value > 10))
 
-# Answer: 
+# Answer: proportion over 10 = 0.654, or 65%
 
-## ----- Question 5: Which store with over $10K in total sales_value discounts its products the most for loyal customers?
+### ----- Question 5: Which store with over $10K in total sales_value discounts
+#### its products the most for loyal customers?
 
 transaction_data %>%
-  select(sales_value, loyalty_price, coupon_disc, regular_price) %>%
-  mutate(pct_loyalty_disc = 1 - (loyalty_price / regular_price)) %>%
-  filter(sales_value, pct_loyalty_disc) %>%
+  filter(
+    is.finite(regular_price), 
+    is.finite(loyalty_price), # whyyyyyyy
+    regular_price > 0
+    ) %>% 
+  mutate(pct_loyalty_disc = 1 - (loyalty_price / regular_price)) %>% 
+  group_by(store_id) %>% 
+  summarize(
+    total_sales_value = sum(sales_value), 
+    avg_pct_loyalty_disc = mean(pct_loyalty_disc)
+  ) %>%
+  filter(total_sales_value > 10000) %>%
+  arrange(desc(avg_pct_loyalty_disc))
+
+# Answer: It's store 341, but I have no idea why the is.finite arguments have to be in there, let alone sum/mean calculations 
+  
+  
   
   
   
