737f38436b141ee522a8c4d751d8855ea1115f4d
Benjamin Goldberg <bgold1996@gmail.com>
2019-03-20 15:43:31 -0400

    Update HW2

--- 01_cj-data-transformation-bdg2ww.R
+++ 01_cj-data-transformation-bdg2ww.R
@@ -1,6 +1,10 @@
 library(tidyverse)
 library(completejourney)
 
-transactions <- transactions
-mutate(transactions, retail_disc = abs(retail_disc), coupon_disc = abs(coupon_disc), coupon_match_disc = abs(coupon_match_disc))
+transactions <- transactions %>% 
+  mutate(
+    retail_disc = abs(retail_disc), 
+    coupon_disc = abs(coupon_disc), 
+    coupon_match_disc = abs(coupon_match_disc)
+)
 
