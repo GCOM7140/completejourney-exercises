8e4500b432d3098434cca2d366ba493c19e0f670
oliversong1 <oliver.song1@gmail.com>
2019-03-20 14:19:53 -0400

    Question 3

--- submissions/01-cj-data-transformation-os8ua.R
+++ submissions/01-cj-data-transformation-os8ua.R
@@ -10,4 +10,15 @@ transactions %>%
          coupon_disc = abs(coupon_disc),
          coupon_match_disc = abs(coupon_match_disc))
 
+## Question 2 - Create three new variables named regular_price, loyalty_price, and coupon_price according to the following logic:
 
+transactions %>%
+  mutate(regular_price = (sales_value + retail_disc + coupon_match_disc) / quantity,
+         loyalty_price = (sales_value + coupon_match_disc) / quantity,
+         coupon_price  = (sales_value - coupon_disc) / quantity)
+
+## Question 3 - The transactions dataset includes 68,509 unique product IDs. How many of these products (not transactions!) had a regular price of one dollar or less? What does this count equal for loyalty price and coupon price?
+
+transactions %>%
+  filter(regular_price <= 1) %>%
+  
