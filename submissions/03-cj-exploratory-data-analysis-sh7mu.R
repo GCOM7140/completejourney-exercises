1a01d98aa8b4816585aa57645e4aaa3a18b1a404
samirhusain96 <sh7mu@virginia.edu>
2019-03-27 11:42:53 -0400

    Samir Husain HW 3

--- submissions/03-cj-exploratory-data-analysis-sh7mu.R
+++ submissions/03-cj-exploratory-data-analysis-sh7mu.R
@@ -45,7 +45,7 @@ transactions_prices %>%
 transactions_prices %>%
   inner_join(demographics, by = "household_id") %>% 
   mutate(
-    household_size = str_replace(household_size, "5\\+", "5") %>% 
+    household_size     = str_replace(household_size, "5\\+", "5") %>% 
       as.integer()
   ) %>% 
   group_by(household_id, week) %>%
@@ -69,10 +69,35 @@ transactions_prices %>%
 #' (i.e., product_type == "BEERALEMALT LIQUORS"). Does the association between
 #' these products offer support for the legend?
  
-
+transactions_prices %>% 
+  inner_join(products, by = "product_id") %>% 
+  mutate(
+    diapers = product_type == "BABY DIAPERS", 
+    beer    = product_type == "BEERALEMALT LIQUORS"
+  ) %>% 
+  group_by(basket_id) %>%
+  summarize(
+    basket_has_diapers = max(diapers), 
+    basket_has_beer    = max(beer)
+  ) %>% 
+  summarize(
+    prop_both   = sum(basket_has_diapers * basket_has_beer == 1, na.rm = TRUE) 
+    / sum(basket_has_diapers == 1, na.rm = TRUE),
+    prob_beer   = mean(basket_has_beer, na.rm = TRUE),
+    diaper_lift = prop_both / prob_beer
+  )
 
 
 #' Q4 - Using a stacked bar chart that is partitioned by income level (i.e.,
 #' income), visualize the total amount of money that households in the Complete
 #' Journey Study spent on national-brand products versus private-label products
 #' (i.e., brand).
+ 
+transactions_prices %>% 
+  left_join(demographics, by = "household_id") %>% 
+  left_join(products, by = "product_id") %>% 
+  group_by(income, brand) %>%
+  summarize(spend_total = sum(price_purchase)) %>% 
+  ggplot(mapping = aes(x = income, y = spend_total, fill = brand)) +
+  geom_col(position = "fill") + 
+  theme(axis.text.x = element_text(angle = 45, hjust = 1))
